e702c308022bbb169902bbc6598bfb8cc51b4503
JoaoYamaguti <105678854+JoaoYamaguti@users.noreply.github.com>
2022-08-28 18:04:29 -0300

    resolving problems

--- js/script.js
+++ js/script.js
@@ -1,86 +1,29 @@
 const historic = document.querySelector('.historic')
 const display = document.querySelector('.display')
 
-let newNum
-let op
-let oldNum = 0
-let total = 0
-
-const btnNumeric = document.querySelectorAll('.numeric')
-btnNumeric.forEach(btn => {
+const btns = document.querySelectorAll('button')
+btns.forEach(btn => {
     btn.addEventListener('click', () => {
-        if(display.innerText == '0') {
-            display.innerHTML = ''
-        }
-        if (display.textContent.length < 8){
-            display.innerHTML += btn.textContent 
-        } else {
-            alert('Limite maximo atingido!!!')
-        }
-    })
-})
-
-
-function addToHistoric (operator) {
-    oldNum = Number(display.textContent)
-    op = "+"
-
-    historic.innerText += operator + display.textContent
-    display.innerText = "0"
-}
-
-function calc (operator){
-    switch (operator) {
-        case "+":
-            return oldNum + newNum
-        case "-":
-            return oldNum - newNum
-        case "*":
-            return oldNum * newNum
-        case "/":
-            return oldNum / newNum
-
-    }
-}
-const btnOperation = document.querySelectorAll('.operation')
-btnOperation.forEach(btn => {
-    btn.addEventListener('click', () => {
-        let displayNum = Number(display.textContent)
-
-        switch (btn.textContent) {
-            case "C":
-                historic.innerText = ""
-                display.innerText = "0"
+        switch (btn.innerText) {
+            case 'C':
+                display.innerText = ""
+                historic.innerHTML = ""
                 break;
-                
-            case "CE":
-                display.innerText = "0"
+            case 'CE':
+                if(display.innerText)
+                    display.innerText = display.innerText.slice(0, -1)
                 break;
-                    
-            case "+":
-
+            case '=':
+                try {
+                    let total = eval(display.innerText)
+                    historic.innerText += `${display.innerText} = ${total} | `
+                    display.innerText = total                   
+                } catch {
+                    display.innerText = 'ERROR!'
+                }
                 break;
-
-            case "-":
-                
+            default:
+                display.innerText += btn.innerText
                 break;
-
-            case "*":
-
-                break;
-
-            case "/":
-
-                break;
-
-            case "%":
-
-                break;
-
-            case "=":
-                calc(op)
-                display.innerText = total
-                break;
-        }
-    })
+    }})
 })
