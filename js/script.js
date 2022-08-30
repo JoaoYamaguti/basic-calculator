644fdd8c908b6524ff61c23b9a5cad96f91bb6ce
JoaoYamaguti <105678854+JoaoYamaguti@users.noreply.github.com>
2022-08-29 23:11:07 -0300

    fix somethings

--- js/script.js
+++ js/script.js
@@ -1,29 +1,58 @@
 const historic = document.querySelector('.historic')
 const display = document.querySelector('.display')
 
+let expression = ''
+
 const btns = document.querySelectorAll('button')
 btns.forEach(btn => {
     btn.addEventListener('click', () => {
         switch (btn.innerText) {
-            case 'C':
+            case 'AC':
                 display.innerText = ""
                 historic.innerHTML = ""
-                break;
-            case 'CE':
+                expression = ''
+                break
+            case 'C':
                 if(display.innerText)
                     display.innerText = display.innerText.slice(0, -1)
-                break;
+                break
+            case '+/-':
+                let displayCotentArray = display.innerText.split('')
+                if (displayCotentArray[0] != '-') {
+                    displayCotentArray.unshift('-')
+                    display.innerText = displayCotentArray.join('')
+                } else if (displayCotentArray[0] == '-') {  
+                    displayCotentArray.splice(0, 1)
+                    display.innerText = displayCotentArray.join('')
+                }
+                break
             case '=':
                 try {
-                    let total = eval(display.innerText)
-                    historic.innerText += `${display.innerText} = ${total} | `
-                    display.innerText = total                   
+                    expression += display.innerText
+                    let total = eval(expression)
+                    if (String(total).length < 8) {
+                        historic.innerText += display.innerText + '=' + total + ' | '
+                        display.innerText = total
+                        expression = ''                  
+                    } else {
+                        display.innerText = 'ERR'
+                    }
                 } catch {
                     display.innerText = 'ERROR!'
                 }
                 break;
-            default:
+        }
+        if (btn.classList[0] == 'numeric')
+            if (display.textContent.length < 8) {
                 display.innerText += btn.innerText
-                break;
-    }})
+            } else {
+                alert('Max 8 digits...')
+            }        
+        if (btn.classList[0] == 'operation') {
+            let op = btn.innerText
+            historic.innerText += display.innerText + op
+            expression += display.innerText + op
+            display.innerText = ''
+        }  
+    })
 })
